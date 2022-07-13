23cc476e9f52ee0e1f26d42f7f3a38ef01744d81
saliltimalsina <salil.ragani@gmail.com>
2022-07-14 00:02:24 +0545

    add to cart from product

--- src/screens/Productdescscreen.js
+++ src/screens/Productdescscreen.js
@@ -40,42 +40,16 @@ export default function Productdescscreen() {
             <div className="col-lg-5 col-md-12 col-md-12">
               <img className="img-fluid w-100 pb-1" src={product.image} />
               <div className="small-img-group">
-                {/* <div className="small-img-col">
-                  <img className="small-img w-100" src={bg1} />
-                </div>
 
-                <div className="small-img-col">
-                  <img className="small-img w-100" src={bg1} />
-                </div>
-
-                <div className="small-img-col">
-                  <img className="small-img w-100" src={bg1} />
-                </div> */}
-
-                {/* <div className="small-img-col">
-                  <img className="small-img w-100" src={bg1} />
-                </div> */}
               </div>
             </div>
-            {/* <div className="card p-2 m-3">
-              <h1>{product.name}</h1>
-
-             
-              <p>{product.description}</p>
-            
-          </div> */}
+           
             <div className="col-lg-6 col-md-12 col-md-12">
               <h6>Home /{product.category}</h6>
               <h3 className="py-4">{product.name}</h3>
 
               <h2>{"$" + product.price}</h2>
 
-              {/* <select className="my-3">
-                <option>Select Size</option>
-                <option>XL</option>
-                <option>XXL</option>
-                <option>L</option>
-              </select> */}
 
               <select
                 className="cart-count"
@@ -92,9 +66,9 @@ export default function Productdescscreen() {
               <button className="buy-btn ml-3" onClick={addtocart}>
                 Add to Cart
               </button>
-              {/* <button className="btn btn-dark" onClick={addtocart}>
+              <button className="btn btn-dark" onClick={addtocart}>
                 Add to cart
-              </button> */}
+              </button>
 
               <h4 className="mt-5 mb-4">Product Details</h4>
 
